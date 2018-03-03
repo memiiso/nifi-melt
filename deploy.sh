333a3801d939ca195453eef165f5c4fbe4a81c03
msimsek <--local>
2018-03-03 12:59:35 +0100

    Init new nifi module that transforms data from a database. To deploy this module run the deploy.sh script in nidi-melt directory.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "## Building the app..."
+mvn clean install
+echo "## Deploy to nifi server..."
+cp -f nifi-melt-nar/target/nifi-melt-nar-1.0-SNAPSHOT.nar /opt/nifi-1.5.0/lib/
+echo "## Restart..."
+/opt/nifi-1.5.0/bin/nifi.sh restart
+echo "## Done"
